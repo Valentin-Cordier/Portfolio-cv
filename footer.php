440c415247dfabf59a75a760a44116b974cb59a9
Valentin-Cordier <v.cordier@simplon-charleville.fr>
2019-05-20 16:29:21 +0200

    slider changer mais pas fini

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="styles/footer.css">
+  <title>Footer</title>
+</head>
+
+<body>
+  <div class="block">@PortfolioCV</div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -9,41 +9,48 @@
 
 <body>
   <?php require_once 'header.php';?>
-  <div class="w3-content w3-display-container">
-  <img class="mySlides" src="img/dev.jpg" style="width:100%">
-  <img class="mySlides" src="img/images.jpg" style="width:100%">
-  <img class="mySlides" src="img/dev.jpg" style="width:100%">
-  <img class="mySlides" src="img/images.jpg" style="width:100%">
-  <button class="button display-left" onclick="plusDivs(-1)">&#10094;</button>
-<button class="button display-right" onclick="plusDivs(+1)">&#10095;</button>
-</div>
+  <section id="slideshow">
+    <div class="container">
+      <div class="c_slider"></div>
+      <div class="slider">
+        <figure>
+          <img src="img/dev.jpg" alt="" width=100% height="600" />
+          <figcaption>Salut</figcaption>
 
-<script>
-var slideIndex = 1;
-showDivs(slideIndex);
+        </figure>
+        <!--
+      -->
+        <figure>
+          <img src="img/images.jpg" alt="" width="100%" height="600" />
+          <figcaption>Hey !</figcaption>
+        </figure>
+        <!--
+    -->
+        <figure>
+          <img src="img/a.jpg" alt="" width="100%" height="600" />
+          <figcaption>Non !</figcaption>
+        </figure>
+        <!--
+  -->
+        <figure>
+          <img srx="img/b.jpg" alt="" width="100%" height="600" />
+          <figcaption>Oui !</figcaption>
+        </figure>
+      </div>
+    </div>
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+    <span id="timeline"></span>
+  </section>
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
-</script>
-<div class="img"><img src="images/logo.png"></div>
+  <div class="img"><img src="img/dev.jpg" style="width: 150px; height: 150px;"></div>
   <div class="main1">
     <div class="presentation"><a href="#">Présentation</a></div>
     <div class="portfolio"><a href="#">Portfolio</a></div>
     <div class="portfolio"><a href="#">Compétences</a></div>
     <div class="contact"><a href="#">Contact</a></div>
   </div>
+
+  <?php include 'footer.php'; ?>
 </body>
 
 </html>
